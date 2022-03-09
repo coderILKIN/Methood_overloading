2f8c3cd1739b8302e45a0b72545cf755ca37a163
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-09 12:48:21 +0400

    1 eded daxil edin. Eger o eded 50den kicikdirse o edede qeder 3-e bolunen, 50-100 araligindadirsa o edede qeder 5e bolunen, 100den boyukdurse o edede qeder 8 e bolunen ededlerin sayini ekrana cixardan bir alqoritm yazin.

---
+++ Task3_metod/Task3_metod/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Task3_metod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int number = 0;
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Divide(number));
+
+        }
+
+
+        public static int Divide(int num)
+        {
+            int tempcost = 0;
+            if (num < 50)
+            {
+                tempcost = 3;
+                Search(num, tempcost);
+            }
+            else if (num >= 50 && num < 100)
+            {
+                tempcost = 5;
+                Search(num, tempcost);
+            }
+            else if (num >= 100)
+            {
+                tempcost = 8;
+                Search(num, tempcost);
+
+            }
+            return Search(num, tempcost);
+
+        }
+
+        public static int Search(int num, int cost)
+        {
+
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % cost == 0)
+                {
+
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
