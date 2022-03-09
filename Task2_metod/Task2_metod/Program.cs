fc1dcecb7281e1044ba62f53ec698de4d27c6a93
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-09 12:45:19 +0400

    Console app-da 2 eded ve bir operator(+,-,*,/) daxil olunur. Secilen operatora uygun riyazi prosesin neticesini ekrana yazdiran bir algoritm yazin

---
+++ Task2_metod/Task2_metod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task2_metod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            char num3 = Convert.ToChar(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(process(num1,num3,num2));
+
+        }
+
+        public static int process(int num1, char num3, int num2)
+            {
+            return num1 * num2;
+
+             }
+
+    }
+}
